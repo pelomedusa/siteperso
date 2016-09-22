7f57594f2c994895d68b4daf4c21ccdba915eda9
Pelomedusa <alfredossic@hotmail.fr>
2016-09-22 13:26:05 +0200

    change category

--- js/global.js
+++ js/global.js
@@ -2,7 +2,37 @@
  * Created by pelomedusa on 21/09/2016.
  */
 
+
+function hideMainMenu(callback){
+    $("#containerMenu").addClass("col-lg-12 col-lg-offset-0").removeClass("col-lg-9 col-lg-offset-2");
+
+    $("#viewer").animate({ height: 0 }, "200", function () {
+        $(this).hide();
+
+    });
+    $(".titlebutton").hide().css({
+        'font-size' : '23px',
+        'line-height' : '50px'
+    });
+    $(".menubutton").css({"margin-top":0, "background-color": "white"}).animate({ height: 50, backgroundColor:"#2980B9" }, "200", function(){
+        $(this).each(function(){
+            $(this).addClass("col-lg-1 col-lg-offset-0").removeClass("col-lg-2 col-lg-offset-1")
+        });
+        $("#firstButton").removeClass("col-lg-offset-0").addClass("col-lg-offset-8");
+        $(".titlebutton").fadeIn();
+        if ($(this).is(":last-child")){callback();}
+    });
+    $("#containerMenu").animate({backgroundColor : "#2980B9"},400);
+
+}
+
+function showProjects() {
+    $("#catProjects").fadeIn();
+}
+
+
 $(function(){
+    $(".category, .titleimgproject").hide();
     $( window ).resize(function() {
         var sizewin = $( window ).width();
         if (sizewin>=1200){document.title = 'lg';}
@@ -18,20 +48,16 @@ $(function(){
     });
 
     $(".menubutton").click(function(){
-        $("#viewer").animate({ height: 0 }, "200", function(){
-            $(this).hide();
-            $(".menubutton").animate({ height: 50 }, "200", function(){
-                $(".titlebutton").css({
-                    'font-size' : '30px',
-                    'line-height' : '50px',
-                });
-                $(this).each(function(){
-                    $(this).addClass("col-lg-1 col-lg-offset-0").removeClass("col-lg-2 col-lg-offset-1")
-                });
-                $("#firstButton").removeClass("col-lg-offset-0").addClass("col-lg-offset-8");
-
-
-            });
+        event.stopImmediatePropagation();
+        hideMainMenu(function () {
+            showProjects();
         });
+    });
+
+    $(".hoverproject").hover(function () {
+        $(this).parent(".imgproject").fadeTo( "fast" , 0.5).find(".titleimgproject").fadeIn("fast");
+
+    }, function () {
+        $(this).fadeTo( "fast" , 1);
     })
 });
